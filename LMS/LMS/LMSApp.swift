b561165b9378568c7f4606208bffa43a6c7db7b9
Aryansingh0810 <ss0854850@gmail.com>
2025-03-22 21:50:06 +0530

    Initial project setup

---
+++ LMS/LMS/LMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  LMSApp.swift
+//  LMS
+//
+//  Created by Aryan Singh on 22/03/25.
+//
+
+import SwiftUI
+
+@main
+struct LMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
